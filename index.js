fd3847816d1a1dc2cf06876cdd3c7e59c07407c3
sakakaka29 <rakaadapa@gmail.com>
2025-06-07 19:30:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = 'SAKA GANTENG'; 
+const instagram = 'anonymous'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
+            + 'FN:Agus\n' // saka ganteng
+            + 'ORG:VVIPINDO;\n' // saka ganteng
             + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
